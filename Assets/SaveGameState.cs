5b6a2938371ff7914bb8b458976976558edc49d5
Nicolae M. <nicolaemargamd@gmail.com>
2025-03-01 03:49:25 +0200

    Added saving system

--- Assets/SaveGameState.cs
+++ Assets/SaveGameState.cs
@@ -1,19 +1,39 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using UnityEngine.UI;
+using System.IO;
 
 public class SaveGameState : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     public Button saveButtonSlot;
-    
+    public string saveFilePath = "Assets/Resources/save.json";
+    public string saveSlotFilePath; // Path for the save slot file
+    private string emptySceneData = "{\"currentSceneId\": \"\"}";
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    // Method that gets called when the save button is pressed
+    public void SaveToSlot()
+    {
+        // Add listener to save button click
+        saveButtonSlot.onClick.AddListener(SaveToSlot);
+        // Check if the main save file exists
+        if (File.Exists(saveFilePath))
+        {
+            // If it exists, copy its content to save_slot{num}.json
+            File.Copy(saveFilePath, saveSlotFilePath, true);
+            File.WriteAllText(saveFilePath, emptySceneData); // Clear and set to empty scene
+
+            Debug.Log("Game state saved to the according save_slot json file");
+        }
+        else
+        {
+            Debug.LogError("save.json not found!");
+        }
     }
 }
